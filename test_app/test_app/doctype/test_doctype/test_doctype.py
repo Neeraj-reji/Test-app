75b2d69ef3e5223c0362abdc335edd0c731342dd
hp <neeraj@faircodetech.com>
2026-05-31 00:54:43 +0530

    added the custom file

---
+++ test_app/test_app/doctype/test_doctype/test_doctype.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, neerajreji and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class testdoctype(Document):
+	pass
